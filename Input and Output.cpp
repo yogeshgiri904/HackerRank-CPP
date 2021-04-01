73717dcfa7c31cb7fed9435a0899d2b1107aaa3e
Yogesh Giri <yogeshgiri904@gmail.com>
2021-04-01 11:17:41 +0530

    2

---
+++ Input and Output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c; 
+        int d = a+b+c;
+ 
+    cout << d << endl;
+    return 0;
+}
