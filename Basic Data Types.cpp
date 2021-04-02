85cadc52392fa46ae8126d7ab38ef98c848d54e8
Yogesh Giri <yogeshgiri904@gmail.com>
2021-04-02 13:00:57 +0530

    3

---
+++ Basic Data Types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    
+    cin >>i>>l>>c>>f>>d;
+    cout <<i<<endl<<l<<endl<<c<<endl;
+    cout <<setprecision(3)<<fixed<<f<<endl;
+    cout <<setprecision(9)<<fixed<<d<<endl;
+
+    return 0;
+    
+}
